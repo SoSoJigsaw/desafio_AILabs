79e2e715c5b3b66c906c460918420c8702e8f59b
Felipe Sobral <felipesobral_@hotmail.com>
2024-01-17 16:43:58 -0300

    Conclusao backend

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
 from flask import Flask, jsonify
+from sugestoesPesquisa import sugestoes_aleatorio, pesquisa_pelo_id, todos_assuntos, pesquisa_pelo_assunto
 
 app = Flask(__name__)
 
@@ -66,6 +67,59 @@ def respostaSugestao(respostaSugestao):
     messages.append({"role": "assistant", "content": answer})
 
     respo = jsonify(answer)
+    respo.headers.add("Access-Control-Allow-Origin", "*")
+    respo.headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+    respo.headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+    return respo
+
+@app.route('/sugestoes-aleatorias', methods=['GET'])
+def sugestoesAleatorias():
+
+    listaSugestoes = sugestoes_aleatorio(6)
+
+    respo = jsonify(listaSugestoes)
+    respo.headers.add("Access-Control-Allow-Origin", "*")
+    respo.headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+    respo.headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+    return respo
+
+
+@app.route('/perguntaid/<int:id>', methods=['GET'])
+def perguntaid(id):
+
+    pergunta = pesquisa_pelo_id(id)
+
+    print(pergunta)
+
+    respo = jsonify(pergunta)
+    respo.headers.add("Access-Control-Allow-Origin", "*")
+    respo.headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+    respo.headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+    return respo
+
+
+@app.route('/assuntos', methods=['GET'])
+def assuntos():
+
+    assuntos = todos_assuntos()
+
+    respo = jsonify(assuntos)
+    respo.headers.add("Access-Control-Allow-Origin", "*")
+    respo.headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+    respo.headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+    return respo
+
+
+@app.route('/perguntaassunto/<string:assunto>', methods=['GET'])
+def pergunta_assunto(assunto):
+
+    perguntas = pesquisa_pelo_assunto(assunto)
+
+    respo = jsonify(perguntas)
     respo.headers.add("Access-Control-Allow-Origin", "*")
     respo.headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
     respo.headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization")
